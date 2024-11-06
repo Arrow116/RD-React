6e006dd793fe57ce1f7fd85e309659552e0b3763
Harikrishnan B <harikrishnan.b@inapp.com>
2024-11-06 13:21:23 +0530

    Initial commit

---
+++ src/VideoPlayer.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef } from "react";
+import Hls from "hls.js";
+
+const VideoPlayer = ({ videoUrl }) => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource(videoUrl);
+      hls.attachMedia(videoRef.current);
+      hls.on(Hls.Events.MANIFEST_PARSED, () => {
+        videoRef.current.play();
+      });
+
+      return () => {
+        hls.destroy();
+      };
+    } else if (videoRef.current.canPlayType('application/vnd.apple.mpegurl')) {
+      videoRef.current.src = videoUrl;
+      videoRef.current.addEventListener('loadedmetadata', () => {
+        videoRef.current.play();
+      });
+    }
+  }, [videoUrl]);
+
+  return (
+    <div>
+      <video
+        ref={videoRef}
+        controls
+        style={{ width: "100%", height: "auto" }}
+        autoPlay
+      />
+    </div>
+  );
+};
+
+export default VideoPlayer;
